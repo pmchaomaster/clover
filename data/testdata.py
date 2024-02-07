4d41877632079b7458dac6e5d9683b7ab6847c57
Pete Chao <pchaosm@gmail.com>
2024-02-06 18:37:31 -0800

    update directory

---
+++ data/testdata.py
@@ -0,0 +1,7 @@
+
+class SearchEngines:
+    engines = [
+        ('http://www.google.com', 'q', 'h3', 'Google'),
+        ('http://www.bing.com', 'q', 'h2', 'Bing'),
+        ('http://search.yahoo.com', 'p', 'h3', 'Yahoo')
+    ]
